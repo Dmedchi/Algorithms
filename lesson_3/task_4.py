2cb6be416183d0220c19eff91e76f41ebbc17785
Dmedichi <dashamatisova@gmail.com>
2018-07-22 01:50:50 +0300

    Корректировка имени переменной Введение переменной для числа 10

--- lesson_3/task_4.py
+++ lesson_3/task_4.py
@@ -3,25 +3,26 @@
 
 from random import random
 
-list = [0] * 10
+N = 10
+list_ = [0] * N
 
-for i in range(10):
-    list[i] = int(random() * 10)
-print(list)
+for i in range(N):
+    list_[i] = int(random() * N)
+print(list_)
 
-number = list[0]
+number = list_[0]
 max_number_of_times = 1
 
-for num in range(10):   #  1 число
+for num in range(N):   #  1 число
     meeting = 1
 
-    for second_number in range(num + 1, 10):  # сравниваем с последующим
-        if list[num] == list[second_number]:
+    for second_number in range(num + 1, N):  # сравниваем с последующим
+        if list_[num] == list_[second_number]:
             meeting += 1
 
     if meeting > max_number_of_times:
         max_number_of_times = meeting
-        number = list[num]
+        number = list_[num]
 
 if max_number_of_times > 1:
     print(f'\n{number} встречается {max_number_of_times} разa.')
