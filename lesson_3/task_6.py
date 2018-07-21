5d574d0cb9c2cdbf8b3993e4be3a964d2e9e9153
Dmedichi <dashamatisova@gmail.com>
2018-07-22 01:52:05 +0300

    Корректировка имени переменной

--- lesson_3/task_6.py
+++ lesson_3/task_6.py
@@ -4,22 +4,22 @@
 
 from random import random
 
-list = []
+list1 = []
 for i in range(10):
-    list.append(int(random() * 100))
-print(list)
+    list1.append(int(random() * 100))
+print(list1)
 
-mn = min(list)
-imn = list.index(mn)
+mn = min(list1)
+imn = list1.index(mn)
 print(f'Min: {mn} [{imn}]')
 
-mx = max(list)
-imx = list.index(mx)
+mx = max(list1)
+imx = list1.index(mx)
 print(f'Max: {mx} [{imx}]')
 
-list2 = list[imn : imx]
+list2 = list1[imn : imx]
 if list2 == []:
-    list2 = list[imx : imn]
+    list2 = list1[imx : imn]
 
 summa = sum(list2) - list2[0]
 print(f'Сумма элементов между min и max: {summa}')
