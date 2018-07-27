4d36952880b69865c25071115aa04889f8c6e5cb
Dmedichi <dashamatisova@gmail.com>
2018-07-27 11:24:16 +0300

    исправлены ошибки

--- lesson_4_(8)/task_1.py
+++ lesson_4_(8)/task_1.py
@@ -1,57 +1,78 @@
 # 5.Вывести на экран коды и символы таблицы ASCII,
 # начиная с символа под номером 32 и заканчивая 127-м включительно.
 # Вывод выполнить в табличной форме: по десять пар «код-символ» в каждой строке.
-
+import random
 import cProfile
 import timeit
 
 def matrix(n):
-    count = 0
-    for i in range(n):
-        print(f' {i}-{chr(i)} | ', end='')
-        count += 1
-        if count % 10 == 0:
-            print()
+    array = [random.randint(32, 128) for i in range(n)]
+    i = 0
+    while i < n:
+        i += 1
+        if array[i] % 10 == 0:
+            return ' '
 
-# print(timeit.timeit('spam = [i for i in range(100)]', number=1000))
-# 10 -   0.0025376916459161703
-# 100 -  0.03062743931304255
-# 1000 - 0.3604174330549176
-
-# timeit:
-# 10 loops, best of 3: 0.0733 usec per loop
-# 100 loops, best of 3: 0.044 usec per loop
-# 1000 loops, best of 3: 0.0366 usec per loop
+        return f' {i}-{chr(i)} | '
+        # print(f' {i}-{chr(i)} | ', end='')
 
 
-cProfile.run('matrix(100)')
+# "task_1.matrix(10)"
+# 100 loops, best of 3: 58.8 usec per loop
+
+# "task_1.matrix(100)"
+# 100 loops, best of 3: 550 usec per loop
+
+# "task_1.matrix(1000)"
+# 100 loops, best of 3: 6.01 msec per loop
+
+
+# cProfile.run('matrix(1000)')
 #           10:
-#   25 function calls in 0.000 seconds
-# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-#   1     0.000    0.000    0.000    0.000 <string>:1(<module>)
-#   1     0.000    0.000    0.000    0.000 task_5.py:8(matrix)
-#   10    0.000    0.000    0.000    0.000 {built-in method builtins.chr}
-#   1     0.000    0.000    0.000    0.000 {built-in method builtins.exec}
-#   11    0.000    0.000    0.000    0.000 {built-in method builtins.print}
-#   1     0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#   57 function calls in 0.000 seconds
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.000    0.000 <string>:1(<module>)
+#        10    0.000    0.000    0.000    0.000 random.py:173(randrange)
+#        10    0.000    0.000    0.000    0.000 random.py:217(randint)
+#        10    0.000    0.000    0.000    0.000 random.py:223(_randbelow)
+#         1    0.000    0.000    0.000    0.000 task_1.py:8(matrix)
+#         1    0.000    0.000    0.000    0.000 task_1.py:9(<listcomp>)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.exec}
+#        10    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#        12    0.000    0.000    0.000    0.000 {method 'getrandbits' of '_random.Random' objects}
+
 
 #           100:
-#    214 function calls in 0.001 seconds
-#  ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-#    1     0.000    0.000    0.001    0.001 <string>:1(<module>)
-#    1     0.000    0.000    0.001    0.001 task_5.py:8(matrix)
-#   100    0.000    0.000    0.000    0.000 {built-in method builtins.chr}
-#    1     0.000    0.000    0.001    0.001 {built-in method builtins.exec}
-#   110    0.001    0.000    0.001    0.000 {built-in method builtins.print}
-#    1     0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#    544 function calls in 0.001 seconds
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.001    0.001 <string>:1(<module>)
+#       100    0.000    0.000    0.001    0.000 random.py:173(randrange)
+#       100    0.000    0.000    0.001    0.000 random.py:217(randint)
+#       100    0.000    0.000    0.000    0.000 random.py:223(_randbelow)
+#         1    0.000    0.000    0.001    0.001 task_1.py:8(matrix)
+#         1    0.000    0.000    0.001    0.001 task_1.py:9(<listcomp>)
+#         1    0.000    0.000    0.001    0.001 {built-in method builtins.exec}
+#       100    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#       139    0.000    0.000    0.000    0.000 {method 'getrandbits' of '_random.Random' objects}
+
 
 #          1000:
-#  2104 function calls in 0.061 seconds
-# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
-#   1      0.000    0.000    0.061    0.061 <string>:1(<module>)
-#   1      0.005    0.005    0.061    0.061 task_5.py:8(matrix)
-#  1000    0.001    0.000    0.001    0.000 {built-in method builtins.chr}
-#   1      0.000    0.000    0.061    0.061 {built-in method builtins.exec}
-#  1100    0.055    0.000    0.055    0.000 {built-in method builtins.print}
-#   1      0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#   5338 function calls in 0.009 seconds
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.009    0.009 <string>:1(<module>)
+#      1000    0.003    0.000    0.006    0.000 random.py:173(randrange)
+#      1000    0.001    0.000    0.007    0.000 random.py:217(randint)
+#      1000    0.002    0.000    0.003    0.000 random.py:223(_randbelow)
+#         1    0.000    0.000    0.009    0.009 task_1.py:8(matrix)
+#         1    0.001    0.001    0.008    0.008 task_1.py:9(<listcomp>)
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.chr}
+#         1    0.000    0.000    0.009    0.009 {built-in method builtins.exec}
+#      1000    0.000    0.000    0.000    0.000 {method 'bit_length' of 'int' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#      1332    0.001    0.000    0.001    0.000 {method 'getrandbits' of '_random.Random' objects}
 
